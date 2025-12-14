a695561555c2c2cf8d8c92877e130fb208cb9d52
guanguans <ityaozm@gmail.com>
2025-12-15 03:18:18 +0800

    refactor(fixers): improve fixers document generation
    - Simplify the fixers document generation logic by removing unnecessary variable assignments.
    - Enhance readability by using method chaining and collection utilities.
    - Ensure the fixers document is correctly formatted and includes necessary details for each fixer.
    
    Signed-off-by: guanguans <ityaozm@gmail.com>

--- src/Support/ComposerScripts.php
+++ src/Support/ComposerScripts.php
@@ -3,6 +3,7 @@
 /** @noinspection EfferentObjectCouplingInspection */
 /** @noinspection PhpDeprecationInspection */
 /** @noinspection PhpInternalEntityUsedInspection */
+/** @noinspection PhpUnused */
 
 declare(strict_types=1);
 
@@ -21,10 +22,12 @@ use Composer\Script\Event;
 use Guanguans\PhpCsFixerCustomFixers\Fixer\AbstractFixer;
 use Guanguans\PhpCsFixerCustomFixers\Fixers;
 use Illuminate\Support\Str;
+use Illuminate\Support\Stringable;
 use PhpCsFixer\Fixer\ConfigurableFixerInterface;
 use PhpCsFixer\Fixer\DeprecatedFixerInterface;
 use PhpCsFixer\Fixer\FixerInterface;
 use PhpCsFixer\Fixer\WhitespacesAwareFixerInterface;
+use PhpCsFixer\FixerConfiguration\FixerOptionInterface;
 use PhpCsFixer\FixerDefinition\CodeSampleInterface;
 use PhpCsFixer\FixerDefinition\VersionSpecificCodeSampleInterface;
 use PhpCsFixer\FixerFactory;
@@ -56,25 +59,16 @@ final class ComposerScripts
         self::requireAutoload($event);
 
         assert_options(\ASSERT_BAIL, 1);
-        $fixersDocument = str_replace(
-            $searches = ['<pre>', '</pre>'],
-            array_map(
-                static fn (string $search): string => htmlspecialchars(
-                    $search,
-                    \ENT_QUOTES | \ENT_SUBSTITUTE | \ENT_HTML401
-                ),
-                $searches
-            ),
-            self::fixersDocument()
-        );
-        $contents = file_get_contents($path = getcwd().\DIRECTORY_SEPARATOR.'README.md');
+
         $updatedContents = preg_replace(
             '#'.preg_quote($start = '<!-- fixers-document:start -->', '#').'(.*?)'
             .preg_quote($end = '<!-- fixers-document:end -->', '#').'#s',
-            $start.\PHP_EOL.$fixersDocument.\PHP_EOL.$end,
-            $contents
+            $start.\PHP_EOL.self::fixersDocument().\PHP_EOL.$end,
+            file_get_contents($path = getcwd().\DIRECTORY_SEPARATOR.'README.md')
         );
+
         \assert(\is_string($updatedContents));
+
         file_put_contents($path, $updatedContents);
 
         $event->getIO()->info('No errors');
@@ -105,161 +99,185 @@ final class ComposerScripts
      */
     private static function fixersDocument(): string
     {
-        if (!\in_array('--dry-run', $_SERVER['argv'], true)) {
-            $_SERVER['argv'][] = '--dry-run';
-        }
-
-        /**
-         * @see https://github.com/laravel/facade-documenter/blob/main/facade.php
-         *
-         * @throws \ReflectionException
-         */
-        $summarizer = static function (AbstractFixer $fixer): string {
-            $summary = $fixer->getDefinition()->getSummary();
-
-            $see = Str::of((new \ReflectionObject($fixer))->getDocComment() ?: '')
-                ->explode("\n")
-                ->skip(1)
-                ->reverse()
-                ->skip(1)
-                ->reverse()
-                ->map(static fn ($line): string => ltrim($line, ' \*'))
-                ->filter(static fn (?string $line): bool => str_starts_with($line, '@see '))
-                ->map(static fn ($line): string => (string) Str::of($line)->after('@see ')->trim())
-                ->values()
-                ->first();
-
-            return false === filter_var($see, \FILTER_VALIDATE_URL)
-                ? $summary
-                : str_replace($aliasName = "`{$fixer->getAliasName()}`", "[$aliasName]($see)", $summary);
-        };
-
-        $differ = static function (string $from, string $to): string {
-            static $differ;
-            $differ ??= new Differ(new StrictUnifiedDiffOutputBuilder([
-                'contextLines' => 1024,
-                'fromFile' => '',
-                'toFile' => '',
-            ]));
-
-            $diff = $differ->diff($from, $to);
-            $start = strpos($diff, "\n", 10);
-            \assert(\is_int($start));
-
-            return (string) substr($diff, $start + 1, -1);
-        };
-
-        $output = '';
-        $fixers = iterator_to_array(new Fixers);
-        usort($fixers, static fn (FixerInterface $a, FixerInterface $b): int => strcmp(\get_class($a), \get_class($b)));
-
-        foreach ($fixers as $fixer) {
-            if ($fixer instanceof WhitespacesAwareFixerInterface) {
-                $fixer->setWhitespacesConfig(new WhitespacesFixerConfig);
-            }
-
-            $output .= \sprintf(
-                "\n<details>\n<summary><b>%s</b></summary>\n\n%s",
-                (new \ReflectionClass($fixer))->getShortName(),
-                $summarizer($fixer),
-            );
-
-            if ($fixer instanceof DeprecatedFixerInterface) {
-                $successors = array_map(
-                    static fn (FixerInterface $fixer): string => $fixer instanceof AbstractFixer
-                        ? (new \ReflectionObject($fixer))->getShortName()
-                        : $fixer->getName(),
-                    (new FixerFactory)
-                        ->registerBuiltInFixers()
-                        ->registerCustomFixers(new Fixers)
-                        ->useRuleSet(new RuleSet(array_combine(
-                            $successorsNames = $fixer->getSuccessorsNames(),
-                            array_pad([], \count($successorsNames), true)
-                        )))
-                        ->getFixers(),
-                );
-
-                $output .= \sprintf("\n\nDeprecated: use `%s` instead.", implode('`, `', $successors));
-            }
-
-            if ($fixer->isRisky()) {
-                $riskyDescription = $fixer->getDefinition()->getRiskyDescription();
-                $starts = [
-                    'Fixer could be risky if' => 'when',
-                    'Risky when' => 'when',
-                ];
-
-                foreach ($starts as $from => $to) {
-                    if (str_starts_with($riskyDescription, $from)) {
-                        $riskyDescription = $to.substr($riskyDescription, \strlen($from));
-                    }
+        return (string) collect(new Fixers)
+            ->tap(static function (): void {
+                if (!\in_array('--dry-run', $_SERVER['argv'], true)) {
+                    $_SERVER['argv'][] = '--dry-run';
                 }
+            })
+            ->sort(static fn (AbstractFixer $a, AbstractFixer $b): int => strcmp(\get_class($a), \get_class($b)))
+            ->reduce(
+                static fn (Stringable $doc, AbstractFixer $fixer): Stringable => $doc
+                    ->tap(static function () use ($fixer): void {
+                        if ($fixer instanceof WhitespacesAwareFixerInterface) {
+                            $fixer->setWhitespacesConfig(new WhitespacesFixerConfig);
+                        }
+                    })
+                    ->append("\n\n<details>")
+                    ->append(
+                        \sprintf("\n<summary><b>%s</b></summary>", (new \ReflectionClass($fixer))->getShortName()),
+                        \sprintf("\n\n%s", self::summaryFor($fixer)),
+                    )
+                    ->when(
+                        $fixer instanceof DeprecatedFixerInterface,
+                        static function (Stringable $doc) use ($fixer): Stringable {
+                            \assert($fixer instanceof DeprecatedFixerInterface);
 
-                $output .= \sprintf(
-                    "\n\nRisky: %s.",
-                    lcfirst(rtrim($riskyDescription, '.')),
-                );
-            }
-
-            if ($fixer instanceof ConfigurableFixerInterface) {
-                $output .= "\n\nConfiguration options:\n";
-
-                foreach ($fixer->getConfigurationDefinition()->getOptions() as $option) {
-                    $allowed = (null !== $option->getAllowedValues())
-                        ? array_map(
-                            static fn (string $value): string => \sprintf("'%s'", $value),
-                            $option->getAllowedValues()
+                            return $doc->append(\sprintf(
+                                "\n\nDeprecated: use `%s` instead.",
+                                collect((new FixerFactory)
+                                    ->registerBuiltInFixers()
+                                    ->registerCustomFixers(new Fixers)
+                                    ->useRuleSet(new RuleSet(array_combine(
+                                        $successorsNames = $fixer->getSuccessorsNames(),
+                                        array_pad([], \count($successorsNames), true)
+                                    )))
+                                    ->getFixers())
+                                    ->map(
+                                        static fn (FixerInterface $fixer): string => $fixer instanceof AbstractFixer
+                                            ? (new \ReflectionObject($fixer))->getShortName()
+                                            : $fixer->getName()
+                                    )
+                                    ->implode('`, `')
+                            ));
+                        }
+                    )
+                    ->when(
+                        $fixer->isRisky(),
+                        static fn (Stringable $doc): Stringable => $doc->append(
+                            \sprintf("\n\nRisky: %s", lcfirst($fixer->getDefinition()->getRiskyDescription()))
                         )
-                        : $option->getAllowedTypes();
+                    )
+                    ->when(
+                        $fixer instanceof ConfigurableFixerInterface,
+                        static function (Stringable $doc) use ($fixer): Stringable {
+                            \assert($fixer instanceof ConfigurableFixerInterface);
 
-                    $output .= \sprintf(
-                        "\n- `%s` (`%s`): %s; defaults to `%s`",
-                        $option->getName(),
-                        implode('`, `', $allowed),
-                        lcfirst(rtrim($option->getDescription(), '.')),
-                        Utils::toString($option->getDefault()),
-                    );
-                }
-            }
+                            return collect($fixer->getConfigurationDefinition()->getOptions())->reduce(
+                                static fn (Stringable $doc, FixerOptionInterface $option): Stringable => $doc->append(
+                                    \sprintf(
+                                        "\n- `%s` (`%s`): %s; defaults to `%s`",
+                                        $option->getName(),
+                                        implode(
+                                            '`, `',
+                                            array_map(
+                                                static fn (string $value): string => "'$value'",
+                                                (array) $option->getAllowedValues()
+                                            ) ?: $option->getAllowedTypes()
+                                        ),
+                                        lcfirst(rtrim($option->getDescription(), '.')),
+                                        Utils::toString($option->getDefault()),
+                                    )
+                                ),
+                                $doc->append("\n\nConfiguration options:\n")
+                            );
+                        }
+                    )
+                    ->pipe(
+                        static fn (Stringable $doc): Stringable => collect($fixer->getDefinition()->getCodeSamples())->reduce(
+                            static function (Stringable $doc, CodeSampleInterface $codeSample) use (&$index, $fixer): Stringable {
+                                $index = ($index ?? 0) + 1;
 
-            $diffs = array_reduce(
-                $fixer->getDefinition()->getCodeSamples(),
-                static function (string $diffs, CodeSampleInterface $codeSample) use ($fixer, $differ): string {
-                    if (
-                        $codeSample instanceof VersionSpecificCodeSampleInterface
-                        && !$codeSample->isSuitableFor(\PHP_VERSION_ID)
-                    ) {
-                        return $diffs;
-                    }
+                                if (
+                                    $codeSample instanceof VersionSpecificCodeSampleInterface
+                                    && !$codeSample->isSuitableFor(\PHP_VERSION_ID)
+                                ) {
+                                    return $doc;
+                                }
 
-                    if ($fixer instanceof ConfigurableFixerInterface) {
-                        $fixer->configure($codeSample->getConfiguration() ?? []);
-                    }
+                                $configuration = [];
 
-                    $code = $codeSample->getCode();
-                    $tokens = Tokens::fromCode($code);
-                    $fixer->fix(
-                        new \SplFileInfo(\sprintf(
-                            '%s%sfile.%s',
-                            getcwd(),
-                            \DIRECTORY_SEPARATOR,
-                            method_exists($fixer, 'randomExtension') ? $fixer->randomExtension() : 'php'
-                        )),
-                        $tokens
-                    );
-                    $fixedCode = $tokens->generateCode();
+                                if ($fixer instanceof ConfigurableFixerInterface) {
+                                    $fixer->configure($configuration = ($codeSample->getConfiguration() ?? []));
+                                }
 
-                    return $diffs.\sprintf(
-                        "\n```diff\n%s\n```\n",
-                        $differ($code, $fixedCode),
-                    );
-                },
-                ''
+                                $tokens = tap(
+                                    Tokens::fromCode($code = $codeSample->getCode()),
+                                    static function (Tokens $tokens) use ($fixer): void {
+                                        $fixer->fix(
+                                            new \SplFileInfo(\sprintf(
+                                                '%s%sfile.%s',
+                                                getcwd(),
+                                                \DIRECTORY_SEPARATOR,
+                                                method_exists($fixer, 'randomExtension') ? $fixer->randomExtension() : 'php'
+                                            )),
+                                            $tokens
+                                        );
+                                    }
+                                );
+
+                                return $doc->append(
+                                    \sprintf(
+                                        "\n\nSample $index: configuration(`%s`)",
+                                        [] === $configuration ? 'default' : Utils::toString($configuration)
+                                    ),
+                                    \sprintf("\n\n```diff\n%s\n```", self::diff($code, $tokens->generateCode()))
+                                );
+                            },
+                            $doc
+                        )
+                    )
+                    ->append("\n</details>"),
+                Str::of('')
+            )
+            ->trim()
+            ->replaceMatches(
+                [
+                    /** @lang PhpRegExp */
+                    '|/opt/homebrew/Cellar/php@\d+\.\d+/.*/bin/php|',
+                    /** @lang PhpRegExp */
+                    '|/opt/homebrew/opt/php@\d+\.\d+/bin/php|',
+                ],
+                'php',
+            )
+            ->replace(
+                $searches = ['<pre>', '</pre>'],
+                array_map(
+                    static fn (string $search): string => htmlspecialchars($search, \ENT_QUOTES | \ENT_SUBSTITUTE),
+                    $searches
+                ),
             );
+    }
 
-            $output .= $diffs ? \sprintf("\n\n%s\n</details>\n", trim($diffs)) : "\n</details>\n";
-        }
+    /**
+     * @see https://github.com/laravel/facade-documenter/blob/main/facade.php
+     *
+     * @throws \ReflectionException
+     */
+    private static function summaryFor(AbstractFixer $fixer): string
+    {
+        $summary = $fixer->getDefinition()->getSummary();
 
-        return trim($output);
+        $see = Str::of((new \ReflectionObject($fixer))->getDocComment() ?: '')
+            ->explode("\n")
+            ->skip(1)
+            ->reverse()
+            ->skip(1)
+            ->reverse()
+            ->map(static fn ($line): string => ltrim($line, ' \*'))
+            ->filter(static fn (?string $line): bool => str_starts_with($line, '@see '))
+            ->map(static fn ($line): string => (string) Str::of($line)->after('@see ')->trim())
+            ->values()
+            ->first();
+
+        return false === filter_var($see, \FILTER_VALIDATE_URL)
+            ? $summary
+            : str_replace($aliasName = "`{$fixer->getAliasName()}`", "[$aliasName]($see)", $summary);
+    }
+
+    private static function diff(string $from, string $to): string
+    {
+        static $differ;
+        $differ ??= new Differ(new StrictUnifiedDiffOutputBuilder([
+            'contextLines' => 1024,
+            'fromFile' => '',
+            'toFile' => '',
+        ]));
+
+        $diff = $differ->diff($from, $to);
+        $start = strpos($diff, "\n", 10);
+        \assert(\is_int($start));
+
+        return (string) substr($diff, $start + 1, -1);
     }
 }
